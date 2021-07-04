5fd1ac2262af72c676889eb0c95381940e03b6a9
uranum-kuri <uranum.kuri@gmail.com>
2021-07-04 15:17:04 +0000

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,16 @@
+if [ -z "${DOT_PATH}" ]; then
+  echo $(tput setaf 1)Deploy dotfiles failed.$(tput sgr0)
+  echo $(tput setaf 1)set DOT_PATH environment variable.$(tput sgr0)
+  exit 1
+fi
+
 cd ${DOT_PATH}/config
 
 for f in .??*
 do
-  [[ ${f} = ".git" ]] && continue
-  [[ ${f} = ".gitignore" ]] && continue
-  ln -snfv ${DOT_PATH}/config/${f} ${HOME}/${f}
+    [[ ${f} = ".git" ]] && continue
+    [[ ${f} = ".gitignore" ]] && continue
+    ln -snfv ${DOT_PATH}/config/${f} ${HOME}/${f}
 done
 
 echo $(tput setaf 2)Deploy dotfiles complete!$(tput sgr0)
