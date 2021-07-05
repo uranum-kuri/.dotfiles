9031b8f1794867eb41714facfc1d28bfc8ffd59d
uranum-kuri <uranum.kuri@gmail.com>
2021-07-05 03:19:58 +0000

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 if [ -z "${DOT_PATH}" ]; then
-  echo $(tput setaf 1)Deploy dotfiles failed.$(tput sgr0)
+  echo $(tput setaf 1)deploy dotfiles failed.$(tput sgr0)
   echo $(tput setaf 1)set DOT_PATH environment variable.$(tput sgr0)
   exit 1
 fi
@@ -13,5 +13,5 @@ do
     ln -snfv ${DOT_PATH}/config/${f} ${HOME}/${f}
 done
 
-echo $(tput setaf 2)Deploy dotfiles complete!$(tput sgr0)
+echo $(tput setaf 2)deploy dotfiles complete! $(tput sgr0)
 
